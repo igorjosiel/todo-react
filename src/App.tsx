9f35c80abb8346ab1a8370fc7cc9f1c34d1df30b
Igor Josiel <igorjosielvitalino@gmail.com>
2025-08-19 21:18:11 -0300

    feat: cria novo componente de botão de ícone.

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import XIcon from "./assets/icons/pencil.svg?react";
 import Icon from "./components/icon";
 import Badge from "./components/badge";
 import Button from "./components/button";
+import ButtonIcon from "./components/button-icon";
 
 function App() {
   return (
@@ -35,6 +36,12 @@ function App() {
 
       <div>
         <Button icon={PlusIcon}>Nova Tarefa</Button>
+      </div>
+
+      <div className="flex gap-1">
+        <ButtonIcon icon={TrashIcon} />
+        <ButtonIcon icon={TrashIcon} variant="secondary" />
+        <ButtonIcon icon={TrashIcon} variant="tertiary" />
       </div>
     </div>
   );
